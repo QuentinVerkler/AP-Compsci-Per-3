f6910b6436388397cb4e2b5830b90770c6c74cf0
QuentinVerkler <43121166+QuentinVerkler@users.noreply.github.com>
2018-10-03 10:17:40 -0700

    Project 2 Ball World 10/03

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -25,6 +25,8 @@ public class BallRunner
         
         }
     }
+    
+    //checks if any indexes of an array are empty
     public int findFreeBallBotIndex(BallBot[] ballBotArray){
         int ballArray = ballBotArray.length;
         for(int i = 0; i<ballBotArray.length; i++){
@@ -41,7 +43,9 @@ public class BallRunner
      BallBot[] ballBotArray = new BallBot[10];
      BallRunner ballRunner = new BallRunner();
      while(true){
+         //first checks if an index is empty
          int freeindex = ballRunner.findFreeBallBotIndex(ballBotArray);
+         //if an index is empty, then it will fill the index with a new ballBot object
          if(freeindex < ballBotArray.length){
              
              ballBotArray[freeindex] = new BallBot(ballWorld, entryPoint, (int)(Math.random())*360, 20);
@@ -59,11 +63,13 @@ public class BallRunner
         }
     }
     
+    //Finds the distance between points
     public double distanceBetweenPoints(TGPoint point1, TGPoint point2){
         double distance = Math.sqrt(Math.pow(point1.x - point2.x , 2) + Math.pow(point1.y - point2.y , 2));
         return distance;
     }
     
+    //checks if an entrance is clear of any ball bots
     public boolean entranceClear(BallBot[] ballBotArray, TGPoint entrancePoint){
         BallRunner ballRunner = new BallRunner();
         boolean question = true;
@@ -84,12 +90,16 @@ public class BallRunner
         BallRunner ballRunner = new BallRunner();
         while(true){
             int x = (int)(Math.random()*20)+1;
+            //first checks if entrance is clear
                 if(ballRunner.entranceClear(ballBotArray, entryPoint) == true){
                     int freeindex = ballRunner.findFreeBallBotIndex(ballBotArray);
+                    //if the entrance is clear, I want to see if a new ballBot is needed.
+                    //if a ballBot is needed, it is created in the array
                     if(freeindex < ballBotArray.length){
                         ballBotArray[freeindex] = new BallBot(ballWorld, entryPoint, (int)(Math.random()*360), x);
                     }
                 }
+                //How balls move around
                         for(int index = 0; index < ballBotArray.length; index++){
                             if(ballBotArray[index] != null){
                                 if(ballBotArray[index].canMoveForward(ballWorld) == true){
@@ -103,7 +113,30 @@ public class BallRunner
                 }
             }
     
+    //method to make a ball bounce if touching another ball
     public BallBot ballBotToBounceOff(BallBot ballBot, BallBot[] ballBotArray){
+        //naming 2 variables, one is where the point is. The other one is where it will move forward
+        TGPoint firstPoint = ballBot.getPoint();
+        TGPoint nextPoint = ballBot.forwardPoint();
+        BallRunner ballRunner = new BallRunner();
+        for(int i = 0; i < ballBotArray.length; i++){
+            BallBot otherBallBot = ballBotArray[i];
+            //checks if otherBallBot is null or not equal to ballBot parameter.
+            //if either true, returns null
+            if(otherBallBot != null & otherBallBot != ballBot){
+                double currentDistance = ballRunner.distanceBetweenPoints(ballBotArray[i].getPoint(), otherBallBot.getPoint());
+                //if the current distance between the two balls is less then their radii combined
+                //then stop. Else, go to next condition
+                if(currentDistance <= ballBot.getRadius() + otherBallBot.getRadius()){
+                    double nextDistance = ballRunner.distanceBetweenPoints(nextPoint, otherBallBot.getPoint());
+                    
+                    if(nextDistance <= currentDistance){
+                        return otherBallBot;
+                    }
+                }
+            }
+        }
+        return null;
         
     }
     

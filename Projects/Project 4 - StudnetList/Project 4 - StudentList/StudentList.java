715bfea982e49fa79a3a5b9405370c9e0458c3ae
QuentinVerkler <43121166+QuentinVerkler@users.noreply.github.com>
2018-10-30 20:14:26 -0700

    Project 4

--- Projects/Project 4 - StudnetList/Project 4 - StudentList/StudentList.java
+++ Projects/Project 4 - StudnetList/Project 4 - StudentList/StudentList.java
@@ -60,26 +60,29 @@ public class StudentList
         System.out.println("Please type in student name or student number");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
+        String checkResponse = " ";
         //checks if name exists
         for(int i = 0; i <= studentList.size(); i++){
             Student stuObject = studentList.get(i);
+            //Parses input into just name
             int endFirstName = name.indexOf(" ");
             int endSecondName = name.indexOf(" ", endFirstName);
-            if(name.indexOf(stuObject.getFullName()) != -1){
-                
+            String section = name.substring(0, endFirstName);
+            String stringNum = Integer.toString(stuObject.getStuNumber());
+            //if the student exists, it will print out all the information of the student
+            if(stuObject.getFullName().indexOf(section) != -1 || stringNum.indexOf(section) != -1){
+                //makes a string that will be used check if a name was found
+                checkResponse = "Name: " + stuObject.getFullName();
+                System.out.println(checkResponse);
+                System.out.println("Student number: " + stuObject.getStuNumber());
+                System.out.println("GPA: " + stuObject.getGPA());
             }
         }
+        //if a name was not found, then print name was not found
+        if(checkResponse.equals(" ") == true){
+            System.out.println("Sorry, that student does not exist in our data base");
+        }
     }
-    public void printStudentList(int stuNum){
-        
-        //chcks if the number exists
-        if(stuNum == 0){
-        System.out.println(lastName + " , " + firstName + " " + middleName);
-        System.out.println(gpa);
-        System.out.println(stuNumber);
-    }else{
-        System.out.println("Student does not exist");
-    }
-    }
+
     
 }
